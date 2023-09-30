30dbc505f05122aa55e312e13b4f6a4a705088ab
Runesave <t.dinh.nice@gmail.com>
2023-09-30 13:57:17 +0200

    V1 almost done. Still lacking the last page Projects

--- script.js
+++ script.js
@@ -1,60 +1,113 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const navLinks = document.querySelectorAll("nav ul li a");
-    const pages = document.querySelectorAll(".page");
-    const menuBtn = document.querySelector(".menu-btn");
-    const navList = document.querySelector(".navbar ul");
-    const activePageText = document.getElementById("active-page");
+  const navLinks = document.querySelectorAll("nav ul li a");
+  const pages = document.querySelectorAll(".page");
+  const menuBtn = document.querySelector(".menu-btn");
+  const navList = document.querySelector(".navbar ul");
+  const activePageText = document.getElementById("active-page");
+  const overlay = document.querySelector('.overlay');
+  const popupButtons = document.querySelectorAll('.popup-button');
 
-    // Fonction pour mettre à jour le texte du bouton de menu
-    function updateMenuButtonText(activePageName) {
-        activePageText.textContent = activePageName;
-    }
+  // Function to update the text of the menu button
+  function updateMenuButtonText(activePageName) {
+    activePageText.textContent = activePageName;
+  }
 
-    // Fonction pour mettre à jour le titre de la page et l'URL
-    function updatePageTitleAndURL(targetPageName) {
-        document.title = "Tam DINH - " + targetPageName;
-        history.pushState({}, "", "#" + targetPageName.toLowerCase());
-    }
+  // Function to update the title of the page and URL
+  function updatePageTitleAndURL(targetPageName) {
+    document.title = "Tam DINH - " + targetPageName;
+    history.pushState({}, "", "#" + targetPageName.toLowerCase());
+  }
 
-    // Affiche la page d'accueil par défaut
-    const defaultPage = document.getElementById("home");
-    defaultPage.style.display = "block";
-    defaultPage.style.opacity = 1;
-    updateMenuButtonText("Home");
-    updatePageTitleAndURL("Home");
-
-    // Écouteur d'événement pour le menu burger
-    menuBtn.addEventListener("click", () => {
-        navList.classList.toggle("active");
+  // Function to show a page by ID
+  function showPage(pageId) {
+    pages.forEach((page) => {
+      page.style.display = "none";
+      page.style.opacity = 0;
     });
 
-    // Écouteur d'événement pour la navigationa
-    navLinks.forEach((link) => {
-        link.addEventListener("click", (event) => {
-            event.preventDefault();
-            const targetId = link.getAttribute("href").substring(1);
+    const targetPage = document.getElementById(pageId);
+    targetPage.style.display = "block";
+    setTimeout(() => {
+      targetPage.style.opacity = 1;
+    }, 10);
+  }
 
-            // Masque toutes les pages avec une animation de transition
-            pages.forEach((page) => {
-                page.style.display = "none";
-                page.style.opacity = 0;
-            });
+  // Show the default page
+  const defaultPageId = "home";
+  showPage(defaultPageId);
+  updateMenuButtonText("Home");
+  updatePageTitleAndURL("Home");
 
-            // Affiche la page cible avec une animation de transition
-            const targetPage = document.getElementById(targetId);
-            targetPage.style.display = "block";
-            setTimeout(() => {
-                targetPage.style.opacity = 1;
-            }, 10);
+  // Event listener for the menu burger
+  menuBtn.addEventListener("click", () => {
+    navList.classList.toggle("active");
+  });
 
-            // Met à jour le texte du bouton du menu avec le nom de la page active
-            updateMenuButtonText(link.textContent);
-
-            // Met à jour le titre de la page et l'URL en fonction de la page active
-            updatePageTitleAndURL(link.textContent);
-            
-            // Ferme le menu burger (pour les petits écrans)
-            navList.classList.remove("active");
-        });
+  // Event listener for navigation links
+  navLinks.forEach((link) => {
+    link.addEventListener("click", (event) => {
+      event.preventDefault();
+      const targetId = link.getAttribute("href").substring(1);
+      showPage(targetId);
+      updateMenuButtonText(link.textContent);
+      updatePageTitleAndURL(link.textContent);
+      navList.classList.remove("active");
     });
+  });
+
+  // Event listener for popup buttons
+  popupButtons.forEach(function (button) {
+    button.addEventListener('click', function () {
+      const popup = this.parentElement.querySelector('.popup-content');
+      popup.style.display = 'block';
+      overlay.style.display = 'block';
+    });
+  });
+
+  // Event listener for closing the pop-up
+  document.addEventListener('click', function (e) {
+    if (e.target.classList.contains('close-popup')) {
+      const popup = e.target.closest('.popup-content');
+      popup.style.display = 'none';
+      overlay.style.display = 'none';
+    }
+  });
+
+  // Event listener to close the pop-up when overlay is clicked
+  overlay.addEventListener('click', function () {
+    popupButtons.forEach(function (button) {
+      const popup = button.parentElement.querySelector('.popup-content');
+      popup.style.display = 'none';
+    });
+    overlay.style.display = 'none';
+  });
+
+  // Event listener for window resize to handle desktop view
+  const $contentWrapper = $("#content-wrapper");
+  const desktopBreakpoint = 768; // Adjust this breakpoint as needed
+
+  $(window).resize(function () {
+    const windowWidth = $(window).width();
+    if (windowWidth >= desktopBreakpoint) {
+      $contentWrapper.removeClass("menu-open");
+      $contentWrapper.css("margin-top", "0");
+    }
+  });
+
+  // Event listener for resume download with permission request
+  const resumeImage = document.querySelector("#resume-link img");
+  resumeImage.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (confirm("Do you want to download the resume?")) {
+      const resumeLink = document.getElementById("resume-link");
+      const resumeHref = resumeLink.getAttribute("href");
+      const a = document.createElement("a");
+      a.href = resumeHref;
+      a.setAttribute("download", "Tam DINH - CV.pdf");
+      a.style.display = "none";
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    }
+  });
 });
